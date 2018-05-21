411880372d314c4f456cf0a4a2bec252d18d5992
dabobmarley@hotmail.com <dabobmarley@hotmail.com>
2018-05-21 09:30:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
-  delete object.key;
+  delete object[key];
  return object;
 }
 function deleteFromObjectByKey(object, key) {
